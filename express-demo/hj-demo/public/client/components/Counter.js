cfbe3d53309f260b4f73f5f9f71186fc028c5212
wanglong <wanglong@hujiang.com>
2017-06-06 17:13:09 +0800

    update:hjdemo

---
+++ express-demo/hj-demo/public/client/components/Counter.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+//UI 组件
+class Counter extends React.Component {
+  componentWillMount(){//在组件装载发生之前立即被调用。 
+    //它在 render() 之前调用，因此在此方法中的设置 state(状态) 不会触发重新渲染。 
+
+  } 
+  constructor(props) {// 类构造函数(class constructor) 初始化 
+    super(props);
+    //组件需要与用户互动，React 就是将组件看成是一个状态机，
+    //一开始有一个初始状态this.state，然后和用户互动，导致状态变化，从而触发重新渲染 UI 
+    //当用户点击组件，导致状态变化，this.setState 方法就是修改状态值，每次修改以后，自动调用 this.render 方法，再次渲染组件。
+
+    //不能直接修改 state(状态)，唯一可以分配 this.state 的地方是构造函数。
+  }
+  componentDidMount(){//在组件输出被渲染到 DOM 之后运行
+     
+  }
+  render() {
+    //输出逻辑：用户发出的动作如何变为 Action 对象，从 UI 组件传出去。它可以是一个函数，也可以是一个对象。做出不同的处理
+    //const { count, onIncreaseClick,onDecreaseClick } = this.props //对象
+    const { count,increase,decrease } = this.props //函数
+    return (
+      <div>
+        <span>{count}</span>
+        <button onClick={increase.bind('add')}>Increase</button>
+        <button onClick={decrease.bind('dec')}>decrease</button>
+      </div>
+    )
+  }
+}
+
+Counter.propTypes = {
+  increase: PropTypes.func.isRequired,
+  decrease: PropTypes.func.isRequired,
+  count: PropTypes.number.isRequired
+}
+
+export default Counter
+
---
+++ express-demo/hj-demo/public/client/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
+import Index from './containers';
+import { h, Component } from 'preact';
+
+const rootEl = document.getElementById('root');
+const store = configureStore()
+//react-redux 提供Provider组件，可以让容器组件拿到state。
+//Provider在根组件外面包了一层，这样一来，根组件的所有子组件就默认都可以拿到state了。
+//render 是 React 的最基本方法，用于将模板转为 HTML 语言，并插入指定的 DOM 节点。
+//再次render()，只附加事件处理程序，从而拥有非常高性能的第一次加载体验。
+render(
+  <Provider store={store}>
+    <Index />
+  </Provider>,
+  rootEl
+)
