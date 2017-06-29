f3c1858422e40a15f6114a33b436679439df12d3
unknown <wanglong@hujiang.com>
2017-06-29 20:03:07 +0800

    Signed-off-by: unknown <wanglong@hujiang.com>

---
+++ demo/asyncAction/views/layout.js
@@ -0,0 +1,32 @@
+const React = require('react');
+
+class Layout extends React.Component {
+  render() {
+    let data = this.props.data;
+    return (
+      <html>
+        <head>
+          <title>{this.props.data.title}</title>
+          <meta charSet='utf-8' />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+          <meta name="keywords" content={this.props.data.keywords} />
+          <meta name="description" content={this.props.data.description} />
+        </head>
+        <body>
+            <div id='root' dangerouslySetInnerHTML={{__html:this.props.data.body}}></div>
+
+            <link rel="stylesheet" href={this.props.data.initStyles} />
+            <script type="text/javascript" async="" src={this.props.data.initScript}></script>
+            <script dangerouslySetInnerHTML={{__html:`console.log("hello world");`}}/>
+        </body>
+      </html>
+    );
+  }
+}
+
+Layout.propTypes = {
+  title: React.PropTypes.string
+};
+
+
+module.exports = Layout;
