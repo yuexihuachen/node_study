c86c88b58a5d75d9952cd0ec7a897ed6eabe1f97
wanglong <wanglong@hujiang.com>
2018-12-10 18:37:45 +0800

    update

--- demo/operate/test.js
+++ demo/operate/test.js
@@ -240,181 +240,181 @@
 
 /**********************************************************/
 
-// var Event = (function () {
+var Event = (function(){
+  var _listen,
+    _trigger,
+    _remove,
+    _shift = Array.prototype.shift,
+    _unshift = Array.prototype.unshift,
+    namespaceCache = {},
+    _create,
+    each = function (ary, fn) {
+      var ret;
+      for (var i = 0, l = ary.length; i < l; i++) {
+        var n = ary[i];
+        ret = fn.call(n, i);
+      }
+      return ret;
+    };
 
-//   var _listen,
-//     _trigger,
-//     _remove,
-//     _shift = Array.prototype.shift,
-//     _unshift = Array.prototype.unshift,
-//     namespaceCache = {},
-//     _create,
-//     each = function (ary, fn) {
-//       var ret;
-//       for (var i = 0, l = ary.length; i < l; i++) {
-//         var n = ary[i];
-//         ret = fn.call(n, i);
-//       }
-//       return ret;
-//     };
+  _listen = function (key, fn, cache) {//订阅的消息添加到缓存列表
+    //cache缓存订阅消息
+    if (!cache[key]) {
+      cache[key] = [];
+    }
+    cache[key].push(fn);
+  };
 
-//   _listen = function (key, fn, cache) {//订阅的消息添加到缓存列表
-//     //cache缓存订阅消息
-//     if (!cache[key]) {
-//       cache[key] = [];
-//     }
-//     cache[key].push(fn);
-//   };
+  _remove = function (key, cache, fn) {//取消订阅事件
+    if (cache[key]) {
+      if (fn) {
+        for (var i = cache[key].length; i >= 0; i--) {
+          if (cache[key][i] === fn) {
+            cache[key].splice[i, 1];
+          }
+        }
+      } else {
+        cache[key] = [];
+      }
+    }
+  };
 
-//   _remove = function (key, cache, fn) {//取消订阅事件
-//     if (cache[key]) {
-//       if (fn) {
-//         for (var i = cache[key].length; i >= 0; i--) {
-//           if (cache[key][i] === fn) {
-//             cache[key].splice[i, 1];
-//           }
-//         }
-//       } else {
-//         cache[key] = [];
-//       }
-//     }
-//   };
+  _trigger = function () {//发布消息
+    var cache = _shift.call(arguments),//拿到缓存的订阅消息列表
+      key = _shift.call(arguments),//发布的消息名称
+      args = arguments,
+      _self = this,
+      stack = cache[key];//从缓存的订阅消息列表拿到当前发布的消息列表
+    //发布的消息没有订阅
+    if (!stack || !stack.length) {
+      return;
+    }
+    //执行发布后对应的订阅消息
+    return each(stack, function () {
+      return this.apply(_self, args);
+    });
+  };
 
-//   _trigger = function () {//发布消息
-//     var cache = _shift.call(arguments),//拿到缓存的订阅消息列表
-//       key = _shift.call(arguments),//发布的消息名称
-//       args = arguments,
-//       _self = this,
-//       stack = cache[key];//从缓存的订阅消息列表拿到当前发布的消息列表
-//     //发布的消息没有订阅
-//     if (!stack || !stack.length) {
-//       return;
-//     }
-//     //执行发布后对应的订阅消息
-//     return each(stack, function () {
-//       return this.apply(_self, args);
-//     });
-//   };
+  _create = function (namespace) {
+    var namespace = namespace || 'default';
+    var cache = {},
+      offlineStack = [],
+      ret = {
+        listen: function (key, fn, last) {
+          _listen(key, fn, cache);
+          //离线的发布信息，未被订阅
+          if (offlineStack === null) {
+            return;
+          }
 
-//   _create = function (namespace) {
-//     var namespace = namespace || 'default';
-//     var cache = {},
-//       offlineStack = [],
-//       ret = {
-//         listen: function (key, fn, last) {
-//           _listen(key, fn, cache);
-//           //离线的发布信息，未被订阅
-//           if (offlineStack === null) {
-//             return;
-//           }
+          if (last === 'last') {
+            offlineStack.length && offlineStack.pop()();
+          } else {
+            each(offlineStack, function () {
+              this();
+            });
+          }
 
-//           if (last === 'last') {
-//             offlineStack.length && offlineStack.pop()();
-//           } else {
-//             each(offlineStack, function () {
-//               this();
-//             });
-//           }
+          offlineStack = null;
+        },
+        one: function (key, fn, last) {
+          _remove(key, cache);
+          this.listen(key, fn, last);
+        },
+        remove: function (key, fn) {
+          _remove(key, cache, fn);
+        },
+        trigger: function () {
+          //发布消息
+          var fn,
+            args,
+            _self = this;
+          _unshift.call(arguments, cache); //缓存的订阅消息列表合并到参数里
+          //新的参数
+          args = arguments;
+          //发布消息
+          fn = function () {
+            return _trigger.apply(_self, args);
+          };
+          //是否有离线的发布消息
+          if (offlineStack) {
+            return offlineStack.push(fn);
+          }
+          //执行发布消息
+          return fn();
+        }
+      };
 
-//           offlineStack = null;
-//         },
-//         one: function (key, fn, last) {
-//           _remove(key, cache);
-//           this.listen(key, fn, last);
-//         },
-//         remove: function (key, fn) {
-//           _remove(key, cache, fn);
-//         },
-//         trigger: function () {
-//           //发布消息
-//           var fn,
-//             args,
-//             _self = this;
-//           _unshift.call(arguments, cache); //缓存的订阅消息列表合并到参数里
-//           //新的参数
-//           args = arguments;
-//           //发布消息
-//           fn = function () {
-//             return _trigger.apply(_self, args);
-//           };
-//           //是否有离线的发布消息
-//           if (offlineStack) {
-//             return offlineStack.push(fn);
-//           }
-//           //执行发布消息
-//           return fn();
-//         }
-//       };
+    return namespace ?
+      (namespaceCache[namespace] ? namespaceCache[namespace] : namespaceCache[namespace] = ret)
+      : ret;
+  };
 
-//     return namespace ?
-//       (namespaceCache[namespace] ? namespaceCache[namespace] : namespaceCache[namespace] = ret)
-//       : ret;
-//   };
+  return {
+    create: _create,
+    one: function (key, fn, last) {
+      var event = this.create();
+      event.one(key, fn, last);
+    },
+    remove: function (key, fn) {
+      var event = this.create();
+      event.remove(key, fn);
+    },
+    listen: function (key, fn, last) {
+      //创建
+      var event = this.create();
+      event.listen(key, fn, last);
+    },
+    trigger: function () {
+      var event = this.create();
+      event.trigger.apply(this, arguments);
+    }
+  };
 
-//   return {
-//     create: _create,
-//     one: function (key, fn, last) {
-//       var event = this.create();
-//       event.one(key, fn, last);
-//     },
-//     remove: function (key, fn) {
-//       var event = this.create();
-//       event.remove(key, fn);
-//     },
-//     listen: function (key, fn, last) {
-//       //创建
-//       var event = this.create();
-//       event.listen(key, fn, last);
-//     },
-//     trigger: function () {
-//       var event = this.create();
-//       event.trigger.apply(this, arguments);
-//     }
-//   };
-
-// })();
-
-
-// Event.create('namespace1').listen('click', function (a) {
-//   console.log(a);
-// });
-
-// Event.create('namespace1').listen('click', function (a) {
-//   console.log(a);
-// });
-
-// Event.create('namespace1').trigger('click', 1);
+})();
 
 
+Event.create('namespace1').listen('click', function (a) {
+  console.log(a);
+});
 
-// Event.create('namespace2').trigger('click',1);
+Event.create('namespace1').listen('click', function (a) {
+  console.log(a);
+});
 
-// Event.create('namespace2').listen('click',function(a){
-//   console.log(a); 
-// });
+Event.create('namespace1').trigger('click', 1);
 
-// Event.create("namespace3").listen("click",function(e){
-// 	console.log(e)
-// })
+
+
+Event.create('namespace2').trigger('click',1);
+
+Event.create('namespace2').listen('click',function(a){
+  console.log(a); 
+});
+
+Event.create("namespace3").listen("click",function(e){
+	console.log(e)
+})
 
 
 /**********************************************************/
 
-var getHelloWord=function() {
-    return "a" + "b";
-}
+// var getHelloWord=function() {
+//     return "a" + "b";
+// }
 
-var obj={
-    getHelloWord:getHelloWord
-}
+// var obj={
+//     getHelloWord:getHelloWord
+// }
 
-let key=Object.keys(obj);
+// let key=Object.keys(obj);
 
-let name={};
+// let name={};
 
-Object.defineProperty(name,key,{
-    get:typeof obj[key] === 'function' 
-    ? obj[key] 
-    : obj[key].get,
-    set:function(){}
-})
+// //get 当访问该属性时，该方法会被执行
+// Object.defineProperty(name,key,{
+//     get:typeof obj[key] === 'function' 
+//     ? obj[key] 
+//     : obj[key].get,
+//     set:function(){}
+// })
